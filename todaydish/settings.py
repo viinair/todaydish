da2917c81eab2cdfd2eb840c9de8bb3e28458902
vipul <vipuln91@gmail.com>
2023-01-23 19:46:40 +0530

    sec

--- todaydish/settings.py
+++ todaydish/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5v6cekaujforf8)5&io#csl_&y&xf$67c^ds)$x(74h7*r9$48'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
