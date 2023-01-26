12ac7cb84723745009ce866372df7709bc1dc7f0
vipul <vipuln91@gmail.com>
2023-01-26 23:43:23 +0530

    headers

--- todaydishApp/views.py
+++ todaydishApp/views.py
@@ -6,10 +6,15 @@ from .models import Dish
 
 def randomDishView(request):
     dish = {"breakfast": random.choice(Dish.objects.all()).name, "lunch": random.choice(
-        Dish.objects.all()).name, "dinner": random.choice(Dish.objects.all()).name}
-
-    return JsonResponse(dish,json_dumps_params={'indent': 2})
-
+        Dish.objects.all()).name, "dinner": random.choice(Dish.objects.all()).name}\
+    
+    headers = {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET,PUT,PATCH,POST,DELETE",
+        "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept"
+    }
+    response = JsonResponse(dish,headers=headers, json_dumps_params={'indent': 2})
+    return response
 
 def welcomeView(request):
     return HttpResponse('Welcome to Today\'s Dish')
