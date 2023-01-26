af6a6f06e42965abbae849a92cf26d3e3de6b959
vipul <vipuln91@gmail.com>
2023-01-26 17:10:18 +0530

    pretty json

--- todaydishApp/views.py
+++ todaydishApp/views.py
@@ -7,7 +7,7 @@ from .models import Dish
 def randomDishView(request):
     dish = {"breakfast": random.choice(Dish.objects.all()).name, "lunch": random.choice(
         Dish.objects.all()).name, "dinner": random.choice(Dish.objects.all()).name}
-    return JsonResponse(dish)
+    return JsonResponse(dish,json_dumps_params={'indent': 2})
 
 
 def welcomeView(request):
